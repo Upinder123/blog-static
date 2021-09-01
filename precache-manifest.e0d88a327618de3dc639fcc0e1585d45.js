6923d72c5ca5b69405d7020e1121a20ce0189fdb
Upinder123 <46994218+Upinder123@users.noreply.github.com>
2021-09-01 23:27:25 +0530

    Add files via upload

---
+++ precache-manifest.e0d88a327618de3dc639fcc0e1585d45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3617bb0428c0ba93641244721c270b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec0e8b45b3ba3d86bdaf",
+    "url": "/static/css/main.5769c892.chunk.css"
+  },
+  {
+    "revision": "2b148801da206e88d457",
+    "url": "/static/js/2.8ae30059.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.8ae30059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0e8b45b3ba3d86bdaf",
+    "url": "/static/js/main.e1c5bd3d.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
